22c36535e50d385e6b1aa8a0b0c3c6c5a13030e0
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-18 21:38:14 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,12 +1,12 @@
 class Song < ApplicationRecord
   include ActiveModel::Validations
   validates :title, presence: true
-  #validates_with TitleValidator (cannot be repeated by same artist in same year)
+  validates_with TitleValidator (cannot be repeated by same artist in same year)
 
   validates :released, presence: true
 
   validates :release_year, presence: true
-  # validates_with ReleaseYearValidator (less than or equal to current year)
+  validates_with ReleaseYearValidator (less than or equal to current year)
 
   validates :artist_name, presence: true
 end
