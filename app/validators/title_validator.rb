fedf67b351a6f274b0cc27af18b0e2b78d02e2d0
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-18 21:37:57 +0000

    Automatically backed up by Learn

--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -1,15 +1,15 @@
 class TitleValidator < ActiveModel::Validator
   def validate(record)
-    unless matching_songs(record)
-      record.errors[:name] << "We're only allowed to have people who work for the company in the database!"
+    unless matching_songs(record).length > 1
+      record.errors[:title] << "Artist already has song of that title for this year"
     end
   end
 
   def matching_songs(record)
     Song.all.select do |song|
-      song.title == title
-      song.artist == artist
-      song.release_year == release_year
+      song.title == record.title
+      song.artist == record.artist
+      song.release_year == record.release_year
     end
   end
 end
