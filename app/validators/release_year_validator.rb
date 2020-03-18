e21d5673c25edd3bc8988b3c49fcd3b6a70b2495
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-18 21:28:16 +0000

    Automatically backed up by Learn

--- app/validators/release_year_validator.rb
+++ app/validators/release_year_validator.rb
@@ -1,7 +1,7 @@
 class ReleaseYearValidator < ActiveModel::Validator
   def validate(record)
     unless record.is_a? Integer && record <= Time.new.year
-      record.errors[:name] << "We're only allowed to have people who work for the company in the database!"
+      record.errors[:release_year] << "Year cannot be later than current year"
     end
   end
 end
