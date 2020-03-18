be82cb8f1fffb790ea5ca11f4db9d390d79b7ddb
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-18 21:04:29 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ApplicationRecord
   include ActiveModel::Validations
   validates :title, presence: true
-  #validates_with TitleValidator 
-  
+  #validates_with TitleValidator
+
 end
