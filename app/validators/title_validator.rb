afe463525c3966c0956900eb19c0a53023814664
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-18 21:36:52 +0000

    Automatically backed up by Learn

--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -1,11 +1,11 @@
 class TitleValidator < ActiveModel::Validator
   def validate(record)
-    unless record.email.match?(/flatironschool.com/)
+    unless matching_songs(record)
       record.errors[:name] << "We're only allowed to have people who work for the company in the database!"
     end
   end
 
-  def matching_songs(title, artist, release_year)
+  def matching_songs(record)
     Song.all.select do |song|
       song.title == title
       song.artist == artist
