b5be194ff43f9af98f063a5ccb84e3fb24f738f0
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-18 21:27:55 +0000

    Automatically backed up by Learn

--- app/validators/release_year_validator.rb
+++ app/validators/release_year_validator.rb
@@ -1,6 +1,6 @@
 class ReleaseYearValidator < ActiveModel::Validator
   def validate(record)
-    unless record.is_a? Integer && record <= Date.today.year
+    unless record.is_a? Integer && record <= Time.new.year
       record.errors[:name] << "We're only allowed to have people who work for the company in the database!"
     end
   end
