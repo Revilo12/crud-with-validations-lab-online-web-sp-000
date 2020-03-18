31729547480df125169f126fe01de4d70588fcd3
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-18 21:45:56 +0000

    Automatically backed up by Learn

--- app/validators/release_year_validator.rb
+++ app/validators/release_year_validator.rb
@@ -1,6 +1,6 @@
 class ReleaseYearValidator < ActiveModel::Validator
   def validate(record)
-    
+
     unless record.release_year < Time.new.year
       record.errors[:release_year] << "Year cannot be later than current year"
     end
