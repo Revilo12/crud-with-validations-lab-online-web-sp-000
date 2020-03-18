2817cc29f2d290239446870f329176a7ce3baa69
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-18 20:58:38 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,16 +7,28 @@ class SongsController < ApplicationController
     set_song
   end
 
+  def new
+    @song = Song.new
+  end
+
   def edit
     set_song
   end
 
+  def update
+    set_song
+    @song.update(params.require(song))
+
   def create
-    @song = Song.new
+
   end
 
   private
   def set_song
     @song = Song.find(params[:id])
+  end
+
+  def set_params
+    params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
   end
 end
